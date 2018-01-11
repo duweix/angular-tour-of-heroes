a95373a1ba8c2ae3781c73ae55172f0f98485c8e
duweix <duweix@126.com>
2018-01-11 17:50:26 +0800

    duweix@126.com

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -29,7 +29,10 @@ export class HeroDetailComponent implements OnInit {
     ngOnInit(): void {
         this.route.paramMap
             .switchMap((params: ParamMap) => this.heroService.getHero(+params.get("id")))
-            .subscribe(hero => this.hero = hero);
+            .subscribe(hero => {
+                console.log(hero);
+                this.hero = hero;
+            });
     }
 
     goBack(): void {
